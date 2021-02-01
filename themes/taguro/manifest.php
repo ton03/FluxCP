189103b914c7524b1075b0977a7e2aaf188a31da
Antonn Esquivel <esquivel.antonn@gmail.com>
2021-01-31 22:08:37 -0500

    TaguRO theme init

---
+++ themes/taguro/manifest.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'inherit'     => 'bootstrap',
+	'author'      => 'Ton',
+	'version'     => '0.1',
+	'description' => 'TaguRO theme for FluxCP',
+	'website'     => 'https://google.com/'
+);
+?>
