acaaf80d8f5d50ce1df72e8e8670f88c2216b98c
Antonn Esquivel <esquivel.antonn@gmail.com>
2021-02-09 00:45:43 -0500

    works

--- config/application.php
+++ config/application.php
@@ -3,7 +3,7 @@
 // the default, and should be changed as needed.
 return array(
 	'ServerAddress'				=> 'localhost',				// This value is the hostname:port under which Flux runs. (e.g., example.com or example.com:80)
-	'BaseURI'					=> '/',						// The base URI is the base web root on which your application lies.
+	'BaseURI'					=> '/FluxCP',						// The base URI is the base web root on which your application lies.
 	'InstallerPassword'			=> 'CentRO!FluxCP!',		// Installer/updater password.
 	'RequireOwnership'			=> true,					// Require the executing user to be owner of the FLUX_ROOT/data/ directory tree? (Better for security)
 															// WARNING: This will be mostly IGNORED on non-POSIX-compliant OSes (e.g. Windows).
@@ -11,7 +11,7 @@ return array(
 	'DefaultCharMapServer'		=> null,
 	'DefaultLanguage'			=> 'en_us',					// Specify the default control panel language (see FLUX_ROOT/lang/ directory for available languages.)
 	'SiteTitle'					=> 'CentRO',	// This value is only used if the theme decides to use it.
-	'ThemeName'					=> array('bootstrap'), // Names of the themes you would like list for use in the footer. Themes are in FLUX_ROOT/themes.
+	'ThemeName'					=> array('centro', 'bootstrap'), // Names of the themes you would like list for use in the footer. Themes are in FLUX_ROOT/themes.
 	'ScriptTimeLimit'			=> 0,						// Script execution time limit. Specifies (in seconds) how long a page should run before timing out. (0 means forever)
 	'MissingEmblemBMP'			=> 'empty.bmp',				//
 	'ItemIconNameFormat'		=> '%d.png',				// The filename format for item icons (defaults to {itemid}.png).
--- themes/centro/manifest.php
+++ themes/centro/manifest.php
@@ -3,7 +3,7 @@ return array(
 	'inherit'     => 'bootstrap',
 	'author'      => 'Ton',
 	'version'     => '0.0.1',
-	'description' => 'ArsenalRO Theme for FluxCP',
-	'website'     => 'http://arsenal-ro.com/'
+	'description' => 'CentRO Theme for FluxCP',
+	'website'     => 'https://centro-ro.com/'
 );
 ?>
