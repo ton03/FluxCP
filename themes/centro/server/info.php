1678d7fbe352d392704599228d3a19021f435177
Antonn Esquivel <esquivel.antonn@gmail.com>
2021-02-14 14:22:21 -0500

    server info

---
+++ themes/centro/server/info.php
@@ -0,0 +1,55 @@
+<?php
+if (!defined('FLUX_ROOT')) exit;
+
+$title = Flux::message('ServerInfoTitle');
+?>
+
+<ul class="ul">
+  <li>Episode: 11.3 Nameless Island</li>
+  <li>Server mode: Pre-Renewal</li>
+  <li>30x Base &amp; Job EXP</li>
+  <li>3x Drop Rate</li>
+  <li>0.2% Card drop rate</li>
+  <li>0.01% Drop rate from MVP/Mini Boss</li>
+</ul>
+
+<h2>Game Protection</h2>
+<ul class="ul">
+  <li>Gepard Shield 3.0</li>
+  <li>Daily backups</li>
+  <li>Custom skill delays</li>
+</ul>
+
+<h2>Server Features</h2>
+<ul class="ul">
+  <li>Max base/job Level: 99/70</li>
+  <li>Max stats: 99</li>
+  <li>Max ASPD: 190</li>
+  <li>MVP maps: PK on</li>
+</ul>
+
+<h2>Party/Guild</h2>
+<ul class="ul">
+  <li>Party size: 15</li>
+  <li>Party share range: 15 levels</li>
+  <li>Guild capacity: 24</li>
+  <li>Guild alliance, MBK, SS, Provoke: Not allowed</li>
+</ul>
+
+<h2>More Features</h2>
+<ul class="ul">
+  <li>Custom "life skills" activities: fishing, farming, cooking</li>
+  <li>Daily missions and tasks</li>
+  <li>Custom UPG equipment mechanics</li>
+  <li>Custom guild vs guild mode</li>
+  <li>Renewal-style equipment refine system</li>
+  <li>No-quest job changer (with level requirement)</li>
+  <li>Reset & Platinum Skills (with zeny requirement)</li>
+</ul>
+
+<h2>Freebies</h2>
+<ul class="ul">
+  <li>Solo Package (obtainable from "New Land" starting quest)</li>
+  <li>Guild Package</li>
+  <li>Streamers Package</li>
+</ul>
