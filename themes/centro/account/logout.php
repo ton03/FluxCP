00b713b0413379a71a94b2c6cfd68bfa81e814a2
Antonn Esquivel <esquivel.antonn@gmail.com>
2021-02-14 20:07:44 -0500

    logout

---
+++ themes/centro/account/logout.php
@@ -0,0 +1,2 @@
+<?php if (!defined('FLUX_ROOT')) exit; ?>
+<p><strong><?php echo htmlspecialchars(Flux::message('LogoutInfo')) ?></strong> <?php printf(Flux::message('LogoutInfo2'), $metaRefresh['location']) ?></p>
